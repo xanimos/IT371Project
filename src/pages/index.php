84f3e563a8ea8d3f12c853aacbed588db01d4aeb
xanimos <dan@xanimos.com>
2023-10-14 22:19:03 -0700

    Add demonstration video

--- src/pages/index.php
+++ src/pages/index.php
@@ -20,19 +20,23 @@ include($_ENV["source_root"].'layouts/header.php');
         </div>
     </section>
     <div class="page-container">
-        <div class="flex-container gap-m">
-            <div class="main-content">
-                <div class="content flex-container gap-m">
-                    <div class="flex-container flex-column gap-m">
-                        <h1 class="title">Welcome to xPressxd</h1>
-                        <p>
-                            This is a place for all of us to gather and share our stories,
-                            knowledge, and just about anything else you would like to.
-                            Together we can create a community of knowledge and intricate worlds.
-                        </p>
-                    </div>
+        <div class="flex-container gap-m main-content flex-column">
+            <div class="content flex-container gap-m">
+                <div class="flex-container flex-column gap-m">
+                    <h1 class="title">Welcome to xPressxd</h1>
+                    <p>
+                        This is a place for all of us to gather and share our stories,
+                        knowledge, and just about anything else you would like to.
+                        Together we can create a community of knowledge and intricate worlds.
+                    </p>
+                </div>
+                <div class="content">
                     <img src="/images/coffe-paper.png" width="100%"/>
                 </div>
+            </div>
+            <div class="content flex-container flex-column flex-center gap-m">
+                <h3>Website demonstration</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/B9xd-8D3VCw?si=ZL3_WrIFZHSysrJK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
     </div>
